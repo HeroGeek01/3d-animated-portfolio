f867e7bd8f32d431eaafdc31e9d3aac4dae10064
HeroGeek01 <141784367+HeroGeek01@users.noreply.github.com>
2025-10-23 18:55:36 +0000

    Inicio TCF

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
+import Hero from "./components/hero/Hero";
+import Services from "./components/services/Services";
+import Portifolio from "./components/portifolio/Portifolio";
+import Contact from "./components/contact/Contact"
+
+
 const App = () => {
   return (
-    <div className=''>Hello World</div>
+    <div className="container">
+      <section id = "#hero">
+        <Hero/>
+      </section>
+      <section id = "#services">
+        <Services/>
+      </section>
+      <section id = "#portifolio">
+         <Portifolio/>
+      </section>
+     <section id = "#contact">
+        <Contact/>
+     </section>
+
+    </div>
   )
 }
 
