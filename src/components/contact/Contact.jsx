773a88abcee362970749e099262f9de0b6606edc
HeroGeek01 <141784367+HeroGeek01@users.noreply.github.com>
2026-04-15 19:00:40 +0000

    Códigos Dia 15/04/2026

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,54 @@
 import "./contact.css";
-
+import emailjs from "@emailjs/browser"
+import { useRef, useState } from "react"
+ 
 const Contact = () => {
-    return <div className = "contact">Contact</div>
+    const [success, setSuccess] = useState(false);
+    const [error, setError] = useState(false);
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(
+                import.meta.env.VITE_SERVICE_ID, 
+                import.meta.env.VITE_TEMPLATE_ID, 
+                form.current, {
+                publicKey: import.meta.env.VITE_PUBLIC_KEY,
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FILED...', error.text);
+                }
+
+            );
+    };
+    return(
+        <div className = "contact" ref={ref} onSubmit={sendEmail}>
+             <div className="cSection">
+                <form>
+                    <h1 className="cTitle">Vamos manter em contato</h1>
+                    <div className="formItem">
+                        <label>Nome</label>
+                        <input type="text" placeholder="Gustavo de Andrade" />
+                    </div>
+                    <div className="formItem">
+                        <label>Email</label>
+                        <input type="email" placeholder="andrade@gmail.com" />
+                    </div>
+                    <div className="formItem">
+                        <label>Mensagem</label>
+                        <textarea rows={10} placeholder="Escreva sua mensagem..."></textarea>
+                    </div>
+                    <button className="formButton">Enviar</button>
+
+                </form>
+             </div>
+             <div className="cSection">SVG</div>
+        </div>
+    )
 };
 
 export default Contact;
