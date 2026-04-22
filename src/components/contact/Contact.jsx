6161c30295d05f67153a5c698f04a341b4155c10
HeroGeek01 <141784367+HeroGeek01@users.noreply.github.com>
2026-04-22 19:01:13 +0000

    Códigos dia 22/04/2026

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,12 @@
 import "./contact.css";
 import emailjs from "@emailjs/browser"
+import { span } from "motion/react-client";
 import { useRef, useState } from "react"
  
 const Contact = () => {
     const [success, setSuccess] = useState(false);
     const [error, setError] = useState(false);
+    const ref = useRef();
     const sendEmail = (e) => {
         e.preventDefault();
 
@@ -12,15 +14,18 @@ const Contact = () => {
             .sendForm(
                 import.meta.env.VITE_SERVICE_ID, 
                 import.meta.env.VITE_TEMPLATE_ID, 
-                form.current, {
+                e.target, {
                 publicKey: import.meta.env.VITE_PUBLIC_KEY,
             })
             .then(
                 () => {
-                    console.log('SUCCESS!');
+                    setSuccess(true);
+                    setError(false);
                 },
                 (error) => {
-                    console.log('FILED...', error.text);
+                    console.log(error);
+                    setError(true);
+                    setSuccess(false);
                 }
 
             );
@@ -32,17 +37,19 @@ const Contact = () => {
                     <h1 className="cTitle">Vamos manter em contato</h1>
                     <div className="formItem">
                         <label>Nome</label>
-                        <input type="text" placeholder="Gustavo de Andrade" />
+                        <input type="text" name="user_username" placeholder="Gustavo de Andrade" />
                     </div>
                     <div className="formItem">
                         <label>Email</label>
-                        <input type="email" placeholder="andrade@gmail.com" />
+                        <input type="email" name = "user_email" placeholder="andrade@gmail.com" />
                     </div>
                     <div className="formItem">
                         <label>Mensagem</label>
-                        <textarea rows={10} placeholder="Escreva sua mensagem..."></textarea>
+                        <textarea rows={10} name="user_message" placeholder="Escreva sua mensagem..."></textarea>
                     </div>
-                    <button className="formButton">Enviar</button>
+                    <button className="formButton" type="submit">Enviar</button>
+                    {success && <span>Sua mensagem foi enviada com sucesso!</span>}
+                    {error && <span>Algo deu errado!</span>}
 
                 </form>
              </div>
